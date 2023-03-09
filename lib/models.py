1bd0178c374fbe62a26b430f233d7bf117ad2a71
Ziru Zou <jiyucoding@yahoo.com>
2023-03-08 22:26:51 -0500

    code along

--- lib/models.py
+++ lib/models.py
@@ -7,6 +7,16 @@ engine = create_engine('sqlite:///many_to_many.db')
 
 Base = declarative_base()
 
+# 4. build associate table - 
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
+
+
 class Game(Base):
     __tablename__ = 'games'
 
@@ -17,6 +27,11 @@ class Game(Base):
     price = Column(Integer())
 
     reviews = relationship('Review', backref=backref('game'))
+
+    # 3. build many-to-many relationship with User,
+    # secondary => the intermediary table in m-m relationship
+    # back_populates => be used on both side of a relationship
+    users = relationship('User', secondary=game_user, back_populates='games')
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
@@ -32,7 +47,32 @@ class Review(Base):
     
     game_id = Column(Integer(), ForeignKey('games.id'))
 
+    # add fk to create relationship with User 
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
     def __repr__(self):
         return f'Review(id={self.id}, ' + \
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
+
+
+
+# 1. Create User model
+class User(Base):
+
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    # 2. setup relationship with Review model
+    reviews = relationship('Review', backref=backref('user'))
+
+    # 3. build many-to-many relationship with Game
+    games = relationship('Game', secondary=game_user, back_populates='users')
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
